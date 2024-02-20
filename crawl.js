17e13d8d3e2566e648d8a9e6e00f7e662828cd27
ant-ny <popadopalus83@gmail.com>
2024-02-20 04:03:49 -0500

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom');
+
 const normalizeURL = (urlString) => {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -7,8 +9,33 @@ const normalizeURL = (urlString) => {
     return hostPath
 }
 
+const getURLsFromHTML = (htmlString, baseURL) => {
+    const urls = []
+    const dom = new JSDOM(htmlString)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
+
 }
